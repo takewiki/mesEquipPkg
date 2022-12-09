734eb190fa94ab4ae7c92d2cf3dde72f48edef3e
takewiki <hulilei@takewiki.com.cn>
2022-12-09 13:13:06 +0800

    initial setup

---
+++ R/mdm.R
@@ -0,0 +1,16 @@
+#' 加法运算
+#'
+#' @param x 一个
+#' @param y 第二个参数
+#'
+#' @return 返回量个数的和
+#' @export
+#'
+#' @examples
+#'myAdd(3,4)
+#'myAdd(5,7)
+myAdd <- function(x,y) {
+  res = x +y
+  return(res)
+
+}
